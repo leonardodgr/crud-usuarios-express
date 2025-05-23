21772e5f72c141b523738763f1e46f16ebf2aa63
Leonardo <xelsleonardo@gmail.com>
2025-05-23 01:07:47 -0300

    Excluindo importações desnecedesnecessárias.

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,6 @@ const express = require('express');
 const dotenv = require('dotenv');
 const cors = require('cors');
 const path = require('path');
-const fs = require('fs');
 
 // Carrega variáveis de ambiente do arquivo .env
 dotenv.config();
